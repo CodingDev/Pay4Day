962ce60329ed2b2a6f22bc057bb0c78259b9185a
René Preuß <rene@preuss.io>
2017-08-02 02:49:58 +0200

    Change version to 5.0-SNAPSHOT Update to SpigotMC 1.12-R0.1-SNAPSHOT Update to VaultAPI 1.6

--- src/main/java/io/preuss/pay4day/Pay4Day.java
+++ src/main/java/io/preuss/pay4day/Pay4Day.java
@@ -1,185 +1,171 @@
-package de.CodingDev.Pay4Day;
-
-import java.io.File;
-import java.io.IOException;
+package io.preuss.pay4day;
 
 import net.milkbowl.vault.economy.Economy;
 import net.milkbowl.vault.economy.EconomyResponse;
 
 import org.bukkit.ChatColor;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import de.CodingDev.Pay4Day.org.mcstats.Metrics;
-
 public class Pay4Day extends JavaPlugin{
-	String prefix = ChatColor.GREEN + "[Pay4Day] ";
-	public File configFile = new File(this.getDataFolder(), "config.yml");
+	private String prefix = ChatColor.GREEN + "[Pay4Day] ";
 	
-	public static Economy econ = null;
+	private static Economy econ = null;
 	
 	public void onDisable(){
 		getLogger().info("Pay4Day has been disabled!");
 	}
 	
 	public void onEnable(){
+		loadConfig();
+		getLogger().info("-------------------------");
+		getLogger().info("Created by: PreuÃŸ.IO GbR");
+		getLogger().info("Website: http://preuss.io/");
+		getLogger().info("Updates: https://dev.bukkit.org/projects/pay4day/");
+		getLogger().info("Authors: " + String.join(", ", getDescription().getAuthors()));
+		getLogger().info("Version: " + getDescription().getVersion());
+		getLogger().info("-------------------------");
 		if (!setupEconomy()){
-			getLogger().warning(String.format("[%s] - Disabled due to no Vault dependency found!", new Object[] { getDescription().getName() }));
+			getLogger().warning(String.format("[%s] - Disabled due to no Vault dependency found! You can download it here https://dev.bukkit.org/projects/vault", new Object[] { getDescription().getName() }));
 			getServer().getPluginManager().disablePlugin(this);
 			return;
 		}
+		getLogger().warning("We have new config.yml! If you use older version than 4.0 PLEASE REMOVE YOUR OLD CONFIG.YML");
+		getLogger().info("-------------------------");
 		getLogger().info("Pay4Day has been enabled!");
-		getLogger().warning("We have new config.yml! If you use older version than 4.0 PLEASE REMOVE OLD CONFIG.YML");
-		loadConfig();
-		getLogger().info("-------------------------");
-		getLogger().info("By: R3N3PDE. New developer Eprimex");
-		getLogger().info("Website: http://codingdev.de/");
-		getLogger().info("Website: http://r3n3p.de/");
-		getLogger().info("Eprimex's website: http://rautamiekka.org");
-		getLogger().info("Updates: http://dev.bukkit.org/server-mods/pay4day/");
-		getLogger().info("Version: " + getDescription().getVersion());
-		getLogger().info("-------------------------");
-		try
-		{
-			Metrics metrics = new Metrics(this);
-			metrics.start();
-		}catch (IOException localIOException) {}
 	}
 	
 	private boolean setupEconomy(){
 		if (getServer().getPluginManager().getPlugin("Vault") == null) {
 			return false;
 		}
-		RegisteredServiceProvider<Economy> rsp = getServer().getServicesManager().getRegistration(Economy.class);
-		if (rsp == null) {
+		RegisteredServiceProvider<Economy> serviceProvider = getServer().getServicesManager().getRegistration(Economy.class);
+		if (serviceProvider == null) {
 			return false;
 		}
-		econ = (Economy)rsp.getProvider();
+		econ = serviceProvider.getProvider();
 		return econ != null;
 	}
 	
-	public boolean onCommand(CommandSender sender, Command command, String commandLabel, String[] args){
+	public boolean onCommand(CommandSender commandSender, Command command, String commandLabel, String[] args){
 		if(command.getName().equalsIgnoreCase("pay4day")){
-			if (!(sender instanceof Player)){
+			if (!(commandSender instanceof Player)){
 				getLogger().info(getConfig().getString("msg." + getConfig().getString("config.language") + ".cmd_command"));
 				return true;
 			}else{
 				if(args.length == 0){
 					//Classic Command
-					if(sender.hasPermission("pay4day.use.classic")){
-						builWeather(sender, "classic", getConfig().getDouble("config.price.classic"));
+					if(commandSender.hasPermission("pay4day.use.classic")){
+						buildWeather(commandSender, "classic", getConfig().getDouble("config.price.classic"));
 					}else{
-						sender.sendMessage(prefix + getConfig().getString("msg." + getConfig().getString("config.language") + ".permissions"));
+						commandSender.sendMessage(prefix + getConfig().getString("msg." + getConfig().getString("config.language") + ".permissions"));
 					}
 				}else if(args.length == 1){
 					if(args[0].equalsIgnoreCase("day")){
-						if(sender.hasPermission("pay4day.use.day")){
-							builWeather(sender, "day", getConfig().getDouble("config.price.day"));
+						if(commandSender.hasPermission("pay4day.use.day")){
+							buildWeather(commandSender, "day", getConfig().getDouble("config.price.day"));
 						}else{
-							sender.sendMessage(prefix + getConfig().getString("msg." + getConfig().getString("config.language") + ".permissions"));
+							commandSender.sendMessage(prefix + getConfig().getString("msg." + getConfig().getString("config.language") + ".permissions"));
 						}
 					}else if(args[0].equalsIgnoreCase("night")){
-						if(sender.hasPermission("pay4day.use.night")){
-							builWeather(sender, "night", getConfig().getDouble("config.price.night"));
+						if(commandSender.hasPermission("pay4day.use.night")){
+							buildWeather(commandSender, "night", getConfig().getDouble("config.price.night"));
 						}else{
-							sender.sendMessage(prefix + getConfig().getString("msg." + getConfig().getString("config.language") + ".permissions"));
+							commandSender.sendMessage(prefix + getConfig().getString("msg." + getConfig().getString("config.language") + ".permissions"));
 						}
 					}else if(args[0].equalsIgnoreCase("sun")){
-						if(sender.hasPermission("pay4day.use.sun")){
-							builWeather(sender, "sun", getConfig().getDouble("config.price.sun"));
+						if(commandSender.hasPermission("pay4day.use.sun")){
+							buildWeather(commandSender, "sun", getConfig().getDouble("config.price.sun"));
 						}else{
-							sender.sendMessage(prefix + getConfig().getString("msg." + getConfig().getString("config.language") + ".permissions"));
+							commandSender.sendMessage(prefix + getConfig().getString("msg." + getConfig().getString("config.language") + ".permissions"));
 						}
 					}else if(args[0].equalsIgnoreCase("rain")){
-						if(sender.hasPermission("pay4day.use.rain")){
-							builWeather(sender, "rain", getConfig().getDouble("config.price.rain"));
+						if(commandSender.hasPermission("pay4day.use.rain")){
+							buildWeather(commandSender, "rain", getConfig().getDouble("config.price.rain"));
 						}else{
-							sender.sendMessage(prefix + getConfig().getString("msg." + getConfig().getString("config.language") + ".permissions"));
+							commandSender.sendMessage(prefix + getConfig().getString("msg." + getConfig().getString("config.language") + ".permissions"));
 						}
 					}else if(args[0].equalsIgnoreCase("storm")){
-						if(sender.hasPermission("pay4day.use.storm")){
-							builWeather(sender, "storm", getConfig().getDouble("config.price.storm"));
+						if(commandSender.hasPermission("pay4day.use.storm")){
+							buildWeather(commandSender, "storm", getConfig().getDouble("config.price.storm"));
 						}else{
-							sender.sendMessage(prefix + getConfig().getString("msg." + getConfig().getString("config.language") + ".permissions"));
+							commandSender.sendMessage(prefix + getConfig().getString("msg." + getConfig().getString("config.language") + ".permissions"));
 						}
 					}
 				}
 			}
 		}else if(command.getName().equalsIgnoreCase("pay4night")){
-			getServer().dispatchCommand(sender, "pay4day night");
+			getServer().dispatchCommand(commandSender, "pay4day night");
 		}else if(command.getName().equalsIgnoreCase("pay4sun")){
-			getServer().dispatchCommand(sender, "pay4day sun");
+			getServer().dispatchCommand(commandSender, "pay4day sun");
 		}else if(command.getName().equalsIgnoreCase("pay4rain")){
-			getServer().dispatchCommand(sender, "pay4day rain");
+			getServer().dispatchCommand(commandSender, "pay4day rain");
 		}else if(command.getName().equalsIgnoreCase("pay4storm")){
-			getServer().dispatchCommand(sender, "pay4day storm");
+			getServer().dispatchCommand(commandSender, "pay4day storm");
 		}
 		return true;
 	}
 	
-	private void builWeather(CommandSender csender, String typ, double price) {
-		Player sender = (Player) csender;
-		EconomyResponse r = econ.withdrawPlayer(sender.getName(), price);
+	private void buildWeather(CommandSender commandSender, String typ, double price) {
+		Player player = (Player) commandSender;
+		EconomyResponse r = econ.withdrawPlayer((OfflinePlayer) commandSender, price);
 		if (r.transactionSuccess()){
 			if(typ.equalsIgnoreCase("classic")){
 				if(getConfig().getBoolean("config.classic.setDay")){
-					sender.getWorld().setTime(0);
+					player.getWorld().setTime(0);
 				}else if(getConfig().getBoolean("config.classic.setNight")){
-					sender.getWorld().setTime(18000);
+					player.getWorld().setTime(18000);
 				}else if(getConfig().getBoolean("config.classic.setSun")){
-					sender.getWorld().setThundering(false);
-					sender.getWorld().setStorm(false);
+					player.getWorld().setThundering(false);
+					player.getWorld().setStorm(false);
 				}else if(getConfig().getBoolean("config.classic.setRain")){
-					sender.getWorld().setThundering(false);
-					sender.getWorld().setStorm(true);
+					player.getWorld().setThundering(false);
+					player.getWorld().setStorm(true);
 				}else if(getConfig().getBoolean("config.classic.setStorm")){
-					sender.getWorld().setThundering(true);
-					sender.getWorld().setStorm(true);
+					player.getWorld().setThundering(true);
+					player.getWorld().setStorm(true);
 				}
 			}else if(typ.equalsIgnoreCase("day")){
-				sender.getWorld().setTime(0);
+				player.getWorld().setTime(0);
 			}else if(typ.equalsIgnoreCase("night")){
-				sender.getWorld().setTime(18000);
+				player.getWorld().setTime(18000);
 			}else if(typ.equalsIgnoreCase("sun")){
-				sender.getWorld().setThundering(false);
-				sender.getWorld().setStorm(false);
+				player.getWorld().setThundering(false);
+				player.getWorld().setStorm(false);
 			}else if(typ.equalsIgnoreCase("rain")){
-				sender.getWorld().setThundering(false);
-				sender.getWorld().setStorm(true);
+				player.getWorld().setThundering(false);
+				player.getWorld().setStorm(true);
 			}else if(typ.equalsIgnoreCase("storm")){
-				sender.getWorld().setThundering(true);
-				sender.getWorld().setStorm(true);
+				player.getWorld().setThundering(true);
+				player.getWorld().setStorm(true);
 			}
 			
-			sender.sendMessage(prefix + getConfig().getString("msg." + getConfig().getString("config.language") + ".surcces_msg_" + typ));
+			player.sendMessage(prefix + getConfig().getString("msg." + getConfig().getString("config.language") + ".surcces_msg_" + typ));
 			
 			if(getConfig().getBoolean("config.sendLocalMsg")){
 				for(Player p : getServer().getOnlinePlayers()){
 					String broadcast = getConfig().getString("msg." + getConfig().getString("config.language") + ".broadcast_" + typ).replace("%price%", String.valueOf(price));
 					broadcast = broadcast.replace("%currency%", getConfig().getString("config.currency"));
-					broadcast = broadcast.replace("%player%", sender.getName());
+					broadcast = broadcast.replace("%player%", player.getName());
 					p.sendMessage(prefix + broadcast);
 				}
 			}
 			
 			String str = getConfig().getString("msg." + getConfig().getString("config.language") + ".surcess_money_msg").replace("%price%", String.valueOf(price));
 			String surcess_money_msg = str.replace("%currency%", getConfig().getString("config.currency"));
-			sender.sendMessage(prefix + surcess_money_msg);
+			player.sendMessage(prefix + surcess_money_msg);
 		}else{
 			String str = getConfig().getString("msg." + getConfig().getString("config.language") + ".surcess_money_msg").replace("%price%", String.valueOf(price));
 			String surcess_money_msg = str.replace("%currency%", getConfig().getString("config.currency"));
-			sender.sendMessage(prefix + surcess_money_msg);
+			player.sendMessage(prefix + surcess_money_msg);
 		}
 	}
 
 	private void loadConfig(){
-		if (!configFile.exists()) {
-			getLogger().info("Config didn't exist. Creating new");
-			getConfig().options().copyDefaults(true);
-		}
 		getConfig().addDefault("config.price.classic", 100.0);
 		getConfig().addDefault("config.price.day", 100.0);
 		getConfig().addDefault("config.price.night", 100.0);
@@ -232,19 +218,19 @@ public class Pay4Day extends JavaPlugin{
 		getConfig().addDefault("msg.de.surcces_msg_rain", "Du hast dir den Regen gekauft.");
 		getConfig().addDefault("msg.de.surcces_msg_storm", "Du hast dir den Sturm gekauft.");
 		
-		getConfig().addDefault("msg.de.broadcast_classic", "%player% hat sich Tag & Sonne gekauft für %price% %currency%.");
-		getConfig().addDefault("msg.de.broadcast_day", "%player% hat sich Tag gekauft für %price% %currency%.");
-		getConfig().addDefault("msg.de.broadcast_night", "%player% hat sich Nacht gekauft für %price% %currency%.");
-		getConfig().addDefault("msg.de.broadcast_sun", "%player% hat sich Sonne gekauft für %price% %currency%.");
-		getConfig().addDefault("msg.de.broadcast_rain", "%player% hat sich Regen gekauft für %price% %currency%.");
-		getConfig().addDefault("msg.de.broadcast_storm", "%player% hat sich Stumr gekauft für %price% %currency%.");
+		getConfig().addDefault("msg.de.broadcast_classic", "%player% hat sich Tag & Sonne gekauft fÃ¼r %price% %currency%.");
+		getConfig().addDefault("msg.de.broadcast_day", "%player% hat sich Tag gekauft fÃ¼r %price% %currency%.");
+		getConfig().addDefault("msg.de.broadcast_night", "%player% hat sich Nacht gekauft fÃ¼r %price% %currency%.");
+		getConfig().addDefault("msg.de.broadcast_sun", "%player% hat sich Sonne gekauft fÃ¼r %price% %currency%.");
+		getConfig().addDefault("msg.de.broadcast_rain", "%player% hat sich Regen gekauft fÃ¼r %price% %currency%.");
+		getConfig().addDefault("msg.de.broadcast_storm", "%player% hat sich Stumr gekauft fÃ¼r %price% %currency%.");
 		
 		getConfig().addDefault("msg.de.surcess_money_msg", "Dir wurden %price% %currency%s abgezogen");
 		getConfig().addDefault("msg.de.not_enough_money", "Du hast zu wenig Geld, du brauchst %price% %currency%.");
-		getConfig().addDefault("msg.de.permissions", "Du hast nicht die Permissions für diesen Befehl.");
-		getConfig().addDefault("msg.de.cmd_command", "Nur Spieler können diesen Befehl benutzen.");
+		getConfig().addDefault("msg.de.permissions", "Du hast nicht die Permissions fÃ¼r diesen Befehl.");
+		getConfig().addDefault("msg.de.cmd_command", "Nur Spieler kÃ¶nnen diesen Befehl benutzen.");
 		
-		//getConfig().options().copyDefaults(true);
+		getConfig().options().copyDefaults(true);
 		saveConfig();
 	}
 }
